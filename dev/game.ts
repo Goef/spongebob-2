4f37af1696e9f380b7fb442ac56946b6ac116060
Orlando Duivestein <0874279@hr.nl>
2018-05-22 00:31:32 +0200

    first commit, added game.ts, tsconfig.json, index.html, and style.js.

---
+++ dev/game.ts
@@ -0,0 +1,24 @@
+class Game{
+    private static instance:Game
+
+    constructor(){
+        this.gameLoop()
+
+    }
+
+    public static getInstance(){
+        if (! Game.instance) {
+            Game.instance = new Game()
+        }
+        return Game.instance
+    }
+
+    private gameLoop():void{
+        requestAnimationFrame(() => this.gameLoop())
+    }
+
+}
+
+window.addEventListener("load", () => {
+    Game.getInstance();
+});
